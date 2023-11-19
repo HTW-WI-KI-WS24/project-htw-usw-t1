f2fd21f6bffe54a54d249ed4df4672fd49266841
endidii <thieuandyhuy@gmail.com>
2023-11-19 13:42:22 +0100

    test file: conversation possible with storing message history

--- openai-test.py
+++ openai-test.py
@@ -1,15 +1,25 @@
 from openai import OpenAI
+
 client = OpenAI()
 
-#temperature 2
+# Initialize conversation with a system message
+messages = [{"role": "system", "content": "You are a helpful assistant."}]
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
-)
+while True:
+    user_text = input("User: ")
 
+    # Add the user message to the conversation history
+    messages.append({"role": "user", "content": user_text})
 
-print(completion.choices[0].message)
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages,
+        temperature=0.5,
+        max_tokens=1000
+    )
+
+    assistant_response = response.choices[0].message.content
+    print(f"\nSystem: {assistant_response}\n")
+
+    # Add grandmas message to the conversation history
+    messages.append({"role": "assistant", "content": assistant_response})
